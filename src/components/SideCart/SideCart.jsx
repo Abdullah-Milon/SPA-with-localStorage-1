86fdf484a2cd442399f788fb84166054e689a21c
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-04-01 01:38:35 +0600

    Project almost  done

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -2,27 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 const SideCart = ({watchTime}) => {
-    
 
+    const [time, setTime] = useState(watchTime);
+    const [breakTime, setBreakTime] = useState(0)
     useEffect( () =>{
         const getWatchTimeFromStorage = localStorage.getItem('watchTime')
         // console.log(getWatchTimeFromStorage)
-        
-    },[])
+            setTime(getWatchTimeFromStorage);
+
+        const getBreakTime = localStorage.getItem('breakTime')
+     /*    if(getBreakTime === null){
+
+        }
+        else{
+
+        } */
+        setBreakTime(getBreakTime);
+            
+    },[watchTime]);
+
+    const handleBreakTime = (bt) =>{
+        // console.log(bt)
+        localStorage.setItem('breakTime', bt)
+        setBreakTime(bt)
+    }
+
+    const handleComplete = () => {
+        toast('wow so easy!!!!!')
+    }
+
 
     return (
         <div className='side-cart'>
             <h1>My Cart</h1>
             <div className="mt-5 text-center">
                 <p>total watchTime</p>
-                <input type="text"  disabled />
+                <input type="text" value={time} disabled />
             </div>
             <h5 className="mt-5">Add break time</h5>
-            <button  className="w-25 btn-circle m-1 btn btn-info">10</button>
-            <button  className="w-25 btn-circle m-1 btn btn-warning">20</button>
-            <button  className="w-25 btn-circle m-1 btn btn-danger">30</button>
-            <input type="text"  disabled />
-            <button  className="mt-5 btn btn-info w-100">Complete</button>
+            <button onClick={() => handleBreakTime(50)} className="w-25 btn-circle m-1 btn btn-info">10</button>
+            <button onClick={() => handleBreakTime(50)} className="w-25 btn-circle m-1 btn btn-warning">20</button>
+            <button onClick={() => handleBreakTime(50)} className="w-25 btn-circle m-1 btn btn-danger">30</button>
+            <input type="text"  value={breakTime} disabled />
+            <button onClick={handleComplete} className="mt-5 btn btn-info w-100">Complete</button>
         </div>
     );
 };
