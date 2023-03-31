e92f794deadcb54517291f9f50d7b2587b4b86ad
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-03-31 21:59:21 +0600

    Design and fetching data step done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import SingleCard from '../SingleCard/SingleCard';
 
-const Home = ({handelWatchTime}) => {
+const Home = () => {
 
-    /* useState মূলত useEffect এ fetch করা data কে সংরক্ষণের জন্য ।
-    এটা লেখার আদর্শ নিয়ম হলো আগে useEffect এ data fetch করে
-     ডাটা লোড করে দেখা তারপর সেই ডাটা সংরক্ষণের জন্য useState কে
-      useEffect এর উপর declare করা */
     const [movies, setMovies] = useState([])
-
-        useEffect( () => {
-            fetch('data.json')
-            .then(res => res.json())
-            .then(data => setMovies(data))
-        },[])
+    useEffect( () =>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data =>setMovies(data))
+    },[])
 
     return (
         <div>
             <div className="movie-container">
-                {
-                    movies.map(movie => <SingleCard
-                    movie = {movie}
-                    handelWatchTime = {handelWatchTime}
-                    ></SingleCard>)
-                }
+                
+                    <SingleCard  ></SingleCard>
+                
             </div>
         </div>
     );
@@ -31,7 +23,3 @@ const Home = ({handelWatchTime}) => {
 
 export default Home;
 
-
-/* useEffect dependency or changeable useEffect bujnai vlo kore jodio 
-milestone-8 conceptualSession-3 a gias vai video 25-30 minite bujyeche 
-pore lagle buje nibo tai alert dea rakhlam  */
--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 
-/* const SingleCard = (props) => {
-    // console.log(props)
-    const {movie} = props;
-    console.log(movie) */
-
-// shortcut kore likhle
-    const   SingleCard = ({ movie, handelWatchTime }) => {
-    // console.log(movie)
+const SingleCard = () => {
 
     return (
         <div className="movie-card card text-center m-auto w-100">
             <div className="movie-poster w-25 m-auto">
-                <img className='w-75' src={movie.poster} alt="" />
+                <img className='w-75' src="https://c8.alamy.com/comp/C2WJYB/the-dark-knight-poster-display-at-the-arclight-theater-los-angeles-C2WJYB.jpg"/>
             </div>
-            <h3>{movie.movieName}</h3>
-            <p>{movie.description}</p>
+            <h3></h3>
+            <p></p>
             <div className="time-rating d-flex justify-content-around">
-                <p>Watch Time:{movie.watchTime}</p>
-                <p>Rating: {movie.imdbRating}</p>
+                <p>Watch Time:</p>
+                <p>Rating: </p>
             </div>
-            <button onClick={()=>handelWatchTime(movie.watchTime)} className='btn btn-info w-50 m-auto'>Book now</button>
+            <button className='btn btn-info w-50 m-auto'>Book now</button>
         </div>
     );
 };
@@ -28,12 +21,6 @@ import React from 'react';
 export default SingleCard;
 
 
-/* SingleCart.jsx এর book now বাটন থেকে যদি sideCard.jsx কিছু পরিবর্তন 
-বা interacitivity করতে চাই তাহলে সরাসরি কিছু করা যাবে না । তাই ক্রমান্বয়ে
- SingleCart.jsx থেকে তার father কম্পনেন্ট home.jsx এ যাবে home.jsx 
- থেকে app.jsx এ যাবে । যেহেতু app.jsx  এর মধ্যে home.jsx ও sideCard.jsx
-  রয়েছে সেহেতু app.jsx প্রত্যাশিত command বা condition sideCart.jsx এর মধ্যে ছড়িয়ে দিবে 
- */
 
 
 
